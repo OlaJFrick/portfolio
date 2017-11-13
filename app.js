216177fae6522b2aacb687c2f01a283813161709
Ola Frick <info@culturehero.org>
2017-11-13 14:04:31 +0100

    Added index.html

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('./www'));
 app.get('*', (req,res) => res.send('Olas uppdaterade Portfolio'));
 
-app.listen(80, () => console.log('Listening on Post 80'));
+app.listen(3000, () => console.log('Listening on Post 3000'));

ffc7d8bd9c09668b6108f361c049829e88b17b09
Ola Frick <info@culturehero.org>
2017-11-16 10:24:33 +0100

    changed a fix in app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./www'));
+app.use(express.static(__dirname + '/www'));
 app.get('*', (req,res) => res.send('Olas uppdaterade Portfolio'));
 
 app.listen(3000, () => console.log('Listening on Post 3000'));
